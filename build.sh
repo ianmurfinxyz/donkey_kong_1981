0534ad345d8cd625a6852a520f80ff31535c9bf0
ianmurfinxyz <ianmurfin@protonmail.com>
2021-02-13 15:49:11 +0000

    initial commit

---
+++ build.sh
@@ -0,0 +1,14 @@
+
+# TODO implement this script
+
+
+#if engine repo ! exist
+#    clone repo
+#build engine repo
+#cp engine bin and include to game/engine (i.e. shared objects and headers)
+#build game
+#cp game output (executable) && engine/assets && game/assets to rootdir/game dir
+#
+#can then play game from within rootdir/game
+#
+#TODO: want to be able to build release and debug builds - how?
